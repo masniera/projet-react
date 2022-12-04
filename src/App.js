1d89975a923908fbd119b440fdb7d787758616d5
Alexandre Masnier <alexandre.masnier@etu.univ-grenoble-alpes.fr>
2022-12-04 23:05:25 +0100

    TP1-2 final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Recettes from "./components/Recettes";
+import React, { useState } from "react";
+import Menu from "./components/Menu";
 
-function App() {
+const App = () => {
+  const [titleToPrint, setTitle] = useState("Appli Recettes de cuisine");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Menu titleToPrint={titleToPrint}/>
     </div>
   );
-}
+};
 
 export default App;
